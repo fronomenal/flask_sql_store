24c4fdd95c38b9621a27ca20427f67c76e0c7c97
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-06-27 23:12:58 +0100

    setup config.py file

---
+++ app/config.py
@@ -0,0 +1,29 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY", "pwrd-key-secret")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../store.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdCon(Config):
+    DEBUG = False
+
+
+class StageCon(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevCon(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+# class TestCon(Config):
+#     TESTING = True
--- app/server.py
+++ app/server.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from app.controllers.middlewares.logger import LogMiddleware
 
 server = Flask(__name__)
-server.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///../store.db'
-server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+server.config.from_object(os.getenv('APP_SETTINGS', "app.config.DevCon"))
 db = SQLAlchemy(server)
 
 
