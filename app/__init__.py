20b687db8e331d26dce768ad693166144475fc06
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-06-27 05:23:21 +0100

    Implemented models with sqlite db

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///store.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///../store.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -12,7 +13,8 @@ def index():
 
 @app.route("/catalog")
 def catalog():
-    return render_template("catalog.html")
+    from app.models.Item import Item
+    return render_template("catalog.html", items=Item.query.all())
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
