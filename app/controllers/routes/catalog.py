5611bdcc251f7cdb5f50926d3c4cd66b7a219cca
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-06-28 17:40:09 +0100

    Changes for heroku staging

--- app/controllers/routes/catalog.py
+++ app/controllers/routes/catalog.py
@@ -2,4 +2,7 @@ from app.server import server, render_template, Item
 
 @server.route("/catalog")
 def catalog():
-    return render_template("catalog.html", items=Item.query.all())
+    #return render_template("catalog.html", items=Item.query.all())
+
+    err = {"stat": 501, "msg": "not yet implemented"}
+    return render_template("reserr.html", err=err)
