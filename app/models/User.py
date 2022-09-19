75a0535a69f67ddc02ca54222abd5c8808cd2d52
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-09-19 23:32:00 +0100

    remigrated database to fix setter error in user model

--- app/models/User.py
+++ app/models/User.py
@@ -5,7 +5,7 @@ class User(db.Model):
     usrname = db.Column(db.String(length=30), nullable=False, unique=True)
     usrbudget = db.Column(db.Integer(), nullable=False, default=999)
     usrmail = db.Column(db.String(length=60), nullable=False, unique=True)
-    usrpass = db.Column(db.String(length=60), nullable=False)
+    _usrpass = db.Column(db.String(length=60), nullable=False)
     items = db.relationship("Item", backref="owned_by", lazy=True)
 
     @property
--- app/server.py
+++ app/server.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app.controllers.middlewares.logger import LogMiddleware
 from flask_bcrypt import Bcrypt
 
 server = Flask(__name__)
 server.config.from_object(os.getenv('APP_SETTINGS', 'app.config.DevCon'))
 db = SQLAlchemy(server)
+migrate = Migrate(server, db)
 bcrypt = Bcrypt(server)
 
 from app.models.Item import Item
